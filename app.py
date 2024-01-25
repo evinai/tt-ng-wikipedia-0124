46a355f40c3690ac7f4688afc605a9e264ada89b
TuncaT <43730654+evinai@users.noreply.github.com>
2024-01-25 11:01:44 +0000

    first

---
+++ app.py
@@ -0,0 +1,27 @@
+import wikipedia
+def search_wikipedia(search_term):
+    try:
+        # Search for the given term on Wikipedia
+        search_results = wikipedia.search(search_term)
+
+        if search_results:
+            # Get the summary of the first search result
+            page_summary = wikipedia.summary(search_results)
+
+            return page_summary
+        else:
+            return "No results found on Wikipedia."
+
+    except wikipedia.exceptions.DisambiguationError as e:
+        # Handle disambiguation error
+        return f"Disambiguation Error: {e.options}"
+
+    except wikipedia.exceptions.PageError:
+        # Handle page not found error
+        return "Page not found on Wikipedia."
+
+# Example usage
+search_term = "Access Control"
+result = search_wikipedia(search_term)
+print(result)
+
